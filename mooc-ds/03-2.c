bdefe4c9a6c5b9cabd8fd07f68e4975fd3e2e8bb
OliverLew <oliver_lew@outlook.com>
2016-05-19 23:25:41 +0800

    new file

---
+++ mooc-ds/03-2.c
@@ -0,0 +1,45 @@
+/**
+ * 03-树2 List Leaves   (25分)
+ * Given a tree, you are supposed to list all the leaves in the order of top 
+ * down, and left to right.
+ * 
+ * Input Specification:
+ * Each input file contains one test case. For each case, the first line gives 
+ * a positive integer N (≤10) which is the total number of nodes in the tree -- 
+ * and hence the nodes are numbered from 0 to N-1. Then N lines follow, each 
+ * corresponds to a node, and gives the indices of the left and right children 
+ * of the node. If the child does not exist, a "-" will be put at the position. 
+ * Any pair of children are separated by a space.
+ * 
+ * Output Specification:
+ * For each test case, print in one line all the leaves' indices in the order 
+ * of top down, and left to right. There must be exactly one space between any 
+ * adjacent numbers, and no extra space at the end of the line.
+ * 
+ * Sample Input:
+ * 
+8
+1 -
+- -
+0 -
+2 7
+- -
+- -
+5 -
+4 6
+ * Sample Output:
+ * 
+ * 4 1 5
+ */
+ 
+/**
+ * tip:
+ * 
+ */
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    
+    return 0;
+}
