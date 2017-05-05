0b04176abf5e6a56aed446cbc63d3d3a5f340402
OliverLew <oliver_lew@outlook.com>
2017-05-05 17:27:05 +0800

    compress code

--- PATBasic/1035.c
+++ PATBasic/1035.c
@@ -41,50 +41,34 @@
 
 int comp(const void *a, const void *b){ return *(int*)a - *(int*)b;}
 
-void MergeStep(int array[], int N, int length)
-{
-    int i;
-    for(i = 0; i < N / length; i++)
-        qsort(array + i * length, length, sizeof(int), comp);
-    qsort(array + i * length, N % length, sizeof(int), comp);
-}
-
-int InsertionLength(int origin[], int halfsort[], int N)
-{
-    int i, length;
-    for(i = 0; i < N - 1 && halfsort[i] <= halfsort[i + 1]; i++) ;
-    for(length = ++i; i < N && halfsort[i] == origin[i]; i++) ;
-    return i == N ? length + 1 : 0;
-}
-
-void MergeLength(int origin[], int halfsort[], int N)
-{
-    int length, i = 0;
-    for(length = 1; i < N && length <= N; length *= 2)
-    {
-        for(i = 0; i < N && origin[i] == halfsort[i]; i++) ;
-        MergeStep(origin, N, length);
-    }
-}
-
 int main()
 {
-    int N, origin[100], halfsort[100], ilength;
+    int N, origin[100], halfsort[100], i, j, length;
     scanf("%d", &N);
     for(int i = 0; i < N; i++) scanf("%d", origin + i);
     for(int i = 0; i < N; i++) scanf("%d", halfsort + i);
     
-    ilength = InsertionLength(origin, halfsort, N);
+    /* if it is insertion sort, return sorted length if yes, zero otherwise */
+    for(i = 0; i < N - 1 && halfsort[i] <= halfsort[i + 1]; i++) ;
+    for(length = ++i; i < N && halfsort[i] == origin[i]; i++) ;
+    length = i == N ? length + 1 : 0;
     
-    if(ilength)
+    if(length)                  /* insertion sort */
     {
         puts("Insertion Sort");
-        qsort(origin, ilength, sizeof(int), comp);
+        qsort(origin, length, sizeof(int), comp);
     }
-    else
+    else                        /* merge sort, operate on the original array */
     {
         puts("Merge Sort");
-        MergeLength(origin, halfsort, N);
+        for(length = 1, i = 0; i < N && length <= N; length *= 2)
+        {
+            /* i == N means identical, also breaks the outer 'for' loop */
+            for(i = 0; i < N && origin[i] == halfsort[i]; i++) ; 
+            for(j = 0; j < N / length; j++)
+                qsort(origin + j * length, length, sizeof(int), comp);
+            qsort(origin + j * length, N % length, sizeof(int), comp);
+        }
     }
     
     for(int i = 0; i < N; i++) 
