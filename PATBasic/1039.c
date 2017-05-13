992e5f8a365ca4dfa449dd7f967a0f76aa2860c0
OliverLew <oliver_lew@outlook.com>
2017-05-13 23:42:53 +0800

    simplify the code

--- PATBasic/1039.c
+++ PATBasic/1039.c
@@ -33,19 +33,18 @@
  * No 2
  */
 #include <stdio.h>
-#include <ctype.h>
 int main()
 {
     char c;
-    int provide[128] = {0}, need[128] = {0};  /* all ASCII characters */
-    while((c = getchar()) != '\n')  provide[(int)c]++;
-    while((c = getchar()) != '\n')  need[(int)c]++;
+    int record[128] = {0};  /* all ASCII characters */
+    while((c = getchar()) != '\n')  record[(int)c]++;
+    while((c = getchar()) != '\n')  record[(int)c]--;
     
     int more = 0, less = 0;
     for(int i = 0; i < 128; i++)
     {
-        if(provide[i] > need[i])    more += provide[i] - need[i];
-        if(provide[i] < need[i])    less += need[i] - provide[i];
+        if(record[i] > 0)    more += record[i];
+        if(record[i] < 0)    less -= record[i];
     }
     
     if(less)    printf("No %d", less);
