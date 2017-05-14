fa437f263cc43b20df2af49ae92cd72145557a3b
OliverLew <oliver_lew@outlook.com>
2017-05-14 15:57:54 +0800

    add 1005

---
+++ PATAdvanced/1005.c
@@ -0,0 +1,37 @@
+/**
+ * 1005. Spell It Right (20)
+ * 
+ * Given a non-negative integer N, your task is to compute the sum of all the 
+ * digits of N, and output every digit of the sum in English.
+ * 
+ * Input Specification:
+ * 
+ * Each input file contains one test case. Each case occupies one line which 
+ * contains an N (<= 10100).
+ * 
+ * Output Specification:
+ * 
+ * For each test case, output in one line the digits of the sum in English 
+ * words. There must be one space between two consecutive words, but no extra 
+ * space at the end of a line.
+ * 
+ * Sample Input:
+ * 12345
+ * Sample Output:
+ * one five
+ */
+#include <stdio.h>
+int main()
+{
+    char c, *digits[] = {"zero", "one", "two", "three", "four", "five",
+                         "six", "seven", "eight", "nine"};
+    int sum = 0;
+    while((c = getchar()) != '\n')
+        sum += c - '0';
+    
+    if(sum >= 100)  printf("%s ", digits[sum / 100]);
+    if(sum >= 10)   printf("%s ", digits[sum % 100 / 10]);
+    printf("%s", digits[sum % 10]);
+    
+    return 0;
+}
