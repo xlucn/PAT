9a5ed705419ddb8ee1656db7726f02bd382a0c79
OliverLew <oliver_lew@outlook.com>
2018-03-03 18:31:45 +0800

    AC code, and simplified some code
    If the total charge for one person is 0, there should be no output. It is required although not clearly pointed out in the text

--- PATAdvanced/1016.c
+++ PATAdvanced/1016.c
@@ -79,49 +79,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ON 1
-#define OFF 0
-#define TIME(P) (((P)->day * 24 + (P)->hour) * 60 + (P)->min)
-
-/* Struct to store phone records */
+/* Structure to store phone records */
 typedef struct {
     char name[21];
-    int month, day, hour, min, state;
+    int month, day, hour, min, time, state;
 }Record, *pRecord;
 
 /* Compare first by name, then by date and time */
 int cmp(const void *record1, const void *record2)
 {
-    pRecord r1 = *(pRecord*)record1;
-    pRecord r2 = *(pRecord*)record2;
-    
-    if(strcmp(r1->name, r2->name))
-        return strcmp(r1->name, r2->name);
-    else if(r1->month - r2->month)
-        return r1->month - r2->month;
-    else if(r1->day - r2->day)
-        return r1->day - r2->day;
-    else if(r1->hour - r2->hour)
-        return r1->hour - r2->hour;
-    else
-        return r1->min - r2->min;
+    pRecord r1 = *(pRecord*)record1, r2 = *(pRecord*)record2;
+    return strcmp(r1->name, r2->name) ? 
+        strcmp(r1->name, r2->name) : r1->time - r2->time;
 }
 
 /* Calculate the charge of the call with start record p1 and end record p2 */
 int calccharge(pRecord p1, pRecord p2, int toll[])
 {
-    /* Convert time to unit of minite */
-    int charge = 0, start = TIME(p1), end = TIME(p2);
-    int h, time1, time2;
+    int charge = 0, start = p1->time, end = p2->time, h, time1, time2;
     
-    /* Add the charge of the call hour by hour */
     for(time1 = start; time1 < end; time1 = time2)
-    {
-        /* time will be the time of next hour */
-        time2 = time1 / 60 * 60 + 60;
-        /* h will be the index of the hour in a day */
-        h = time1 / 60 % 24;
-        /* Add the charge within this hour */
+    {   /* Add the charge hour by hour */
+        time2 = (time1 / 60 + 1) * 60; /* time2 will be the time of next hour */
+        h = time1 / 60 % 24;           /* h will be the index of the hour */
         charge += ((time2 > end ? end : time2) - time1) * toll[h];
     }
     
@@ -144,34 +124,37 @@ int main()
         *p = records + i;
         scanf("%s %d:%d:%d:%d %s", (*p)->name, 
               &(*p)->month, &(*p)->day, &(*p)->hour, &(*p)->min, state);
-        (*p)->state = strcmp(state, "on-line") ? OFF : ON;
+        (*p)->time = ((*p)->day * 24 + (*p)->hour) * 60 + (*p)->min;
+        (*p)->state = strcmp(state, "on-line") ? 0 : 1;
     }
     
     /* Sort first by name, then by date and time */
     qsort(precords, N, sizeof(pRecord), cmp);
     
     /* Print phone bill one by one */
-    printf("%s %02d\n", precords[0]->name, precords[0]->month);
     for(p = precords + 1; *p; p++)
     { 
-        if(strcmp((*p)->name, (*(p - 1))->name))      /* A new customer */
-        {
-            printf("Total amount: $%.2f\n", charge_total * 1e-2);
-            printf("%s %02d\n", (*p)->name, (*p)->month);
+        if(strcmp((*p)->name, (*(p - 1))->name))
+        {            /* A new customer, print last total amount if any */
+            if(charge_total)
+                printf("Total amount: $%.2f\n", charge_total * 1e-2);
             charge_total = 0;
         }
-        else if((*(p - 1))->state == ON && (*p)->state == OFF)
+        else if((*(p - 1))->state == 1 && (*p)->state == 0)
         {            /* Still the same customer, finding on/off record pair */
+            if(charge_total == 0)
+                printf("%s %02d\n", (*p)->name, (*p)->month);
             charge = calccharge(*(p - 1), *p, toll);
             charge_total += charge;
             /* Print info of this call */
             printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", 
                    (*(p - 1))->day, (*(p - 1))->hour, (*(p - 1))->min,
                    (*p)->day, (*p)->hour, (*p)->min,
-                   TIME(*p) - TIME(*(p - 1)), charge * 1e-2);  
+                   (*p)->time - (*(p - 1))->time, charge * 1e-2);  
         }
     }
-    printf("Total amount: $%.2f\n", charge_total * 1e-2);
+    if(charge_total)
+        printf("Total amount: $%.2f\n", charge_total * 1e-2);
     
     return 0;
 }
