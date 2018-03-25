e0f06802ebccc8ca5b960f84467e3ca87f006e5a
Oliver Lew <oliver_lew@outlook.com>
2018-03-25 15:33:25 +0800

    add newest problems to config file

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 indexes = {
     'a': [i + 1001 for i in range(110)] +
          [i + 1101 for i in range(39)],
-    'b': [i + 1001 for i in range(80)],
+    'b': [i + 1001 for i in range(85)],
     't': [i + 1001 for i in range(21)]
 }
 
