30b319722c1312f9529dc936c2919d5d7d9ffe43
Oliver Lew <oliver_lew@outlook.com>
2018-03-26 22:35:14 +0800

    update indexes and added comments

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 #! /usr/bin/env python3
 
-indexes = {
-    'a': [i + 1001 for i in range(110)] +
-         [i + 1101 for i in range(39)],
+indexes = {                 # Problem indexes for each level
+    'a': [i + 1001 for i in range(147)],
     'b': [i + 1001 for i in range(85)],
-    't': [i + 1001 for i in range(21)]
+    't': [i + 1001 for i in range(23)]
 }
 
-html_dir = "html"
+md_dir = "md"               # The dir of final generated markdown file
 
-md_dir = "md"
+html_dir = "html"           # The dir of html file from patest.cn
 
-analysis_dir = "analysis"
+analysis_dir = "analysis"   # The dir of md files of only analysis text
 
-code_dir = "../PAT"
+code_dir = "../PAT"         # Where the code is(the master branch)
 
 configs = {
-    "quote_text": True
+    "quote_text": True  # If add '> ' to problem text in markdown file
 }
