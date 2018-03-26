87728e6b1bffa722270fec0a0453c18dc06cfb6d
Oliver Lew <oliver_lew@outlook.com>
2018-03-26 19:25:11 +0800

    1084.c third version, shortened code

--- PATBasic/1084.c
+++ PATBasic/1084.c
@@ -6,7 +6,11 @@
  * d, d1, d111, d113, d11231, d112213111, ...
  *
  * 它从不等于 1 的数字 d 开始，序列的第 n+1 项是对第 n 项的描述。比如第 2 项表示
+<<<<<<< Updated upstream
  * 第 1 项有 1 个 d，所以就是 d1；第 2 项是 1 个 d（对应 d1）和 1 个 1（对应
+=======
+ * 第 1 项有 1 个 d，所以就是 d1；第 2 项是 1 个 d（对应 d1）和 1 个 1（对应 
+>>>>>>> Stashed changes
  * 11），所以第 3 项就是 d111。又比如第 4 项是 d113，其描述就是 1 个 d，2 个 1，
  * 1 个 3，所以下一项就是 d11231。当然这个定义对 d = 1 也成立。本题要求你推算任
  * 意给定数字 d 的外观数列的第 N 项。
@@ -41,17 +45,14 @@ int main()
     
     for(int i = 1; i < N; i++)  /* Loop through nth string */
     {
-        for(p1 = s1 + 1, p2 = s2, count = 1; *(p1 - 1); p1++)
+        for(p1 = s1 + 1, p2 = s2, count = 0; *(p1 - 1); p1++)
         {
+            count++;
             if(*p1 != *(p1 - 1))    /* New char or end */
             {
                 *p2++ = *(p1 - 1);  /* Record into s2 */
                 *p2++ = count + '0';
-                count = 1;          /* Reset count */
-            }
-            else                    /* Same char, increase count */
-            {
-                count++;
+                count = 0;          /* Reset count */
             }
         }
         
