15cd6cc2f780a424894c48d28aae8feebf52983f
Oliver Lew <oliver_lew@outlook.com>
2018-03-29 16:26:18 +0800

    update code style

--- PATBasic/1064.c
+++ PATBasic/1064.c
@@ -32,13 +32,16 @@ int main()
     for(int i = 0; i < N; i++)
     {
         scanf("%d", &m);
-        for(sum = 0; m; m /= 10) sum += m % 10;
-        if(!friendID[sum]) {friendID[sum] = 1; count++;}
+        for(sum = 0; m; m /= 10) 
+            sum += m % 10;
+        if(!friendID[sum]) 
+            friendID[sum] = 1, count++;
     }
     
     printf("%d\n", count);
-    for(int i = 1; i < 37; i++) if(friendID[i])
-        printf("%d%c", i, --count ? ' ' : '\0');
+    for(int i = 1; i < 37; i++) 
+        if(friendID[i])
+            printf("%d%c", i, --count ? ' ' : '\0');
     
     return 0;
 }
