ee5810ad4abb62cacc989d0825f90d8a9f880828
Oliver Lew <oliver_lew@outlook.com>
2018-04-25 14:48:12 +0800

    improve code style

--- build.py
+++ build.py
@@ -4,9 +4,7 @@ import os
 import sys
 import re
 from subprocess import run, PIPE, CalledProcessError
-from bs4 import BeautifulSoup
 import config
-
 
 class FileBuilder:
     """
@@ -14,8 +12,7 @@ class FileBuilder:
     """
     def __init__(self):
         self.github = "https://github.com/OliverLew/PAT/blob/master"
-        
-    
+
     def read_html(self):
         """
         Open the html file and read into lines
@@ -23,24 +20,24 @@ class FileBuilder:
         html = os.path.join(config.html_dir, "{}{}.html".format(self.c, self.i))
         lines = open(html).readlines()
         return lines[2], lines[3:]
-    
+
     def read_code(self):
         """
         read code file from master branch
         """
-        code_rel_path = os.path.join(config.code_dir[self.c], 
+        code_rel_path = os.path.join(config.code_dir[self.c],
                                      "{}.c".format(self.i))
         github_file_url = os.path.join(self.github, code_rel_path)
-        raw_code = run(["git", "show", "master:" + code_rel_path], check=True, 
+        raw_code = run(["git", "show", "master:" + code_rel_path], check=True,
                        stdout=PIPE, stderr=PIPE).stdout.decode("utf-8")
         code = raw_code[raw_code.index("#include"):]
         return code, github_file_url
-    
+
     def read_expl(self):
         """
         read explanation
         """
-        expl_file = os.path.join(config.analysis_dir, 
+        expl_file = os.path.join(config.analysis_dir,
                                  "{}{}.md".format(self.c, self.i))
         return open(expl_file).read()
 
@@ -52,12 +49,12 @@ class FileBuilder:
         h1_tag, problem_div = self.read_html()
         code, code_url = self.read_code()
         expl = self.read_expl()
-        
+
         print("Building {}".format(filename))
-        
+
         with open(filename, 'w') as f:
             f.write("{}\n".format(h1_tag))
-            
+
             # write problem content
             f.write("## 题目\n\n")
             for line in problem_div:
@@ -65,30 +62,30 @@ class FileBuilder:
                     f.write("> ")
                 f.write(line)
             f.write("\n\n")
-            
+
             # write explanation
             f.write("## 思路\n\n{}\n".format(expl))
-            
+
             # write code
             f.write("## 代码\n\n")
             f.write("[最新代码@github]({})，欢迎交流\n".format(code_url))
             f.write("```c\n{}\n```".format(code))
-      
-    def build(self, category, index):
-        self.c = category
-        self.i = index
+
+    def build(self, c, i):
+        self.c = c
+        self.i = i
         try:
             self.__build()
-        except FileNotFoundError as e:
+        except FileNotFoundError:
             pass
-        except CalledProcessError as e:
+        except CalledProcessError:
             pass
 
 usage = """to be continued."""
 
 if __name__ == "__main__":
     builder = FileBuilder()
-    if(not os.path.exists(config.md_dir)):
+    if not os.path.exists(config.md_dir):
         os.mkdir(config.md_dir)
     if len(sys.argv) == 1:
         for c in list(config.indexes.keys()):
