ab5c5438e394560776bd45f920a156b21f873aec
Oliver Lew <oliver_lew@outlook.com>
2018-05-09 16:36:08 +0800

    add a1020, not tested due to PAT website migration

---
+++ PATAdvanced/1020.c
@@ -0,0 +1,100 @@
+/**
+ * 1020. Tree Traversals (25)
+ * 
+ * Suppose that all the keys in a binary tree are distinct positive integers. 
+ * Given the postorder and inorder traversal sequences, you are supposed to 
+ * output the level order traversal sequence of the corresponding binary tree.
+ * 
+ * Input Specification:
+ * 
+ * Each input file contains one test case. For each case, the first line gives a
+ * positive integer N (<=30), the total number of nodes in the binary tree. The
+ * second line gives the postorder sequence and the third line gives the inorder
+ * sequence. All the numbers in a line are separated by a space.
+ * 
+ * Output Specification:
+ * 
+ * For each test case, print in one line the level order traversal sequence of 
+ * the corresponding binary tree. All the numbers in a line must be separated by
+ * exactly one space, and there must be no extra space at the end of the line.
+ * Sample Input:
+ * 
+ * 7
+ * 2 3 1 5 7 6 4
+ * 1 2 3 4 5 6 7
+ * 
+ * Sample Output:
+ * 
+ * 4 1 6 3 5 7 2
+ **/
+
+#include <stdio.h>
+
+#define QLEN 15
+#define CNODE 31
+
+typedef struct node{
+    int *post, *in, length;
+}node;
+
+typedef struct queue{
+    node *nodes[QLEN];
+    int front, rear, count;
+}queue;
+
+void enqueue(queue *q, node *n)
+{
+    q->nodes[q->rear] = n;
+    q->rear = (q->rear == QLEN - 1) ? 0 : q->rear + 1;
+    q->count++;
+}
+
+node *dequeue(queue *q)
+{
+    node *n = q->nodes[q->front];
+    q->front = (q->front == QLEN - 1) ? 0 : q->front + 1;
+    q->count--;
+    return n;
+}
+
+int main()
+{
+    int post[CNODE] = {0}, in[CNODE] = {0}, N;
+    queue q = {0};  /* Initialize to zeros or NULL array */
+    int root, index, count;
+    
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++)
+        scanf("%d", post + i);
+    for(int i = 0; i < N; i++)
+        scanf("%d", in + i);
+    
+    node nodes[CNODE] = {{post, in, N}}, *p = nodes, *n;
+    enqueue(&q, p++);
+    for(count = 0; q.count; count++)
+    {
+        n = dequeue(&q);
+        /* Find root node in post-order, which is the last one */
+        root = n->post[n->length - 1];
+        /* Find the root node in in-order */
+        for(index = 0; index < n->length; index++)
+            if(n->in[index] == root)
+                break;
+        /* The subsequence post/in-order of left child */
+        p->post = n->post;
+        p->in = n->in;
+        p->length = index;
+        if(p->length != 0)              /* left child not NULL */
+            enqueue(&q, p++);
+        /* The subsequence post/in-order of right child */
+        p->post = n->post + index;
+        p->in = n->in + index + 1;
+        p->length = n->length - index - 1;
+        if(p->length != 0)              /* right child not NULL */
+            enqueue(&q, p++);
+        /* print */
+        printf("%d%c", root, (count == N - 1) ? '\0' : ' ');
+    }
+    
+    return 0;
+}
