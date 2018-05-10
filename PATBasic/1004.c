6d5a412f438a1c290accdac8d4faf4500904e246
Oliver Lew <oliver_lew@outlook.com>
2018-05-10 22:23:53 +0800

    replace tabs with spaces

--- PATBasic/1004.c
+++ PATBasic/1004.c
@@ -37,7 +37,7 @@ int main()
     scanf("%d", &N);
     
     char maxname[11], minname[11], curname[11], 
-		maxid[11], minid[11], curid[11];
+        maxid[11], minid[11], curid[11];
     int maxgrade = -1, mingrade = 101, curgrade;
     
     for(int i = 0; i < N; i++)
