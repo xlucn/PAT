f2fcc493a3829b80ccdba28675f7a9508c76ac3b
Oliver Lew <oliver_lew@outlook.com>
2018-06-14 19:47:04 +0800

    a1019: all test points passed

--- PATAdvanced/1019.c
+++ PATAdvanced/1019.c
@@ -51,14 +51,13 @@
 
 int main()
 {
-    int N, b, c, isPalindromicNumber = 1;
-    char b_base[30] = {0};
+    int N, b, c, isPalindromicNumber = 1, b_base[30] = {0};
 
     scanf("%d %d", &N, &b);
 
-    /* Calculate N in b-base, actually storing the number reversely */
-    for(c = 0; N; N /= b)
-        b_base[c++] = N % b;
+    /* Calculate N in b-base */
+    for(c = 0; N; N /= b, c++)
+        b_base[c] = N % b;
 
     /* Check if it is palindromic number */
     for(int i = 0; i < c / 2; i++)
@@ -69,7 +68,7 @@ int main()
     if(c == 0)
         printf("0");
     /* Reversely printing is the correct order */
-    while(c--)      /* not --c */
+    while(c--)
         printf("%d%c", b_base[c], c ? ' ' : '\0');
 
     return 0;
