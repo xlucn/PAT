4aad1dea92eb12ca39fba404491916c519481b7c
Oliver Lew <oliver_lew@outlook.com>
2018-09-16 14:13:52 +0800

    add basic 1087

---
+++ PATBasic/1087.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, n, m, m0 = -1, count = 0;
+    
+    scanf("%d", &N);
+    
+    for(n = 1; n <= N; n++)
+    {
+        m = n / 2 + n / 3 + n / 5;
+        if(m > m0)
+            count++;
+        m0 = m;
+    }
+    
+    printf("%d", count);
+    
+    return 0;
+}
