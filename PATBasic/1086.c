1eaeefcff935f61aafae7c6c7c1695ff448746b3
Oliver Lew <oliver_lew@outlook.com>
2018-10-08 19:53:24 +0800

    add b1086

---
+++ PATBasic/1086.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    scanf("%d %d", &A, &B);
+    C = A * B;
+    
+    while(!(C % 10))
+        C /= 10;
+    
+    while(C)
+    {
+        putchar('0' + C % 10);
+        C /= 10;
+    }
+    
+    return 0;
+}
