282b682bcf442a4d75019f6a2a1e90ccf21e426b
Oliver Lew <oliver_lew@outlook.com>
2018-12-23 20:27:21 +0800

    update markdown files

--- source/config.py
+++ source/config.py
@@ -28,9 +28,9 @@ cat_string = {
 }
 
 indexes = {                 # Problem indexes for each level
-    'a': [i + 1001 for i in range(147)],
-    'b': [i + 1001 for i in range(85)],
-    't': [i + 1001 for i in range(23)]
+    'a': [i + 1001 for i in range(155)],
+    'b': [i + 1001 for i in range(95)],
+    't': [i + 1001 for i in range(27)]
 }
 
 quote_text = False   # If add '> ' to problem text in markdown file
